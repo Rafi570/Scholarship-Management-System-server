0722b9da3676710200cb5c7e29653318f81dbff6
Rafi570 <hasan.cse570@gmail.com>
2025-12-06 02:21:10 +0600

    user api done

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// Connect to MongoDB
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB!");
+
+    // Example route using MongoDB
+    const db = client.db("Scholarship-Management-System");
+    const userCollection = db.collection("user");
+
+    // user related Api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      user.role = "student";
+      user.createdAt = new Date();
+      const email = user.email;
+      const userExists = await userCollection.findOne({ email });
+
+      if (userExists) {
+        return res.send({ message: "user exists" });
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+run();
+
+// Default route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
